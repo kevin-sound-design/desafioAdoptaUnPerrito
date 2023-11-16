70bb5ac3d4a7dca9f230e1fe8c8f1db0c57d7f87
DESKTOP-IS2G159\kevin <kevin.rodfa@gmail.com>
2023-11-15 21:26:25 -0300

    detalle arreglado

--- src/components/Tags.jsx
+++ src/components/Tags.jsx
@@ -1,6 +1,6 @@
 function Badge({color, texto}){
     const colorTemplate =`badge text-bg-${color}`
-    return <span class={colorTemplate}>{texto}</span>
+    return <span className={colorTemplate}>{texto}</span>
 }
 
 export default Badge
